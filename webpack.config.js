59a05c2b77f5cc55cf8314edb0e63d468427dabd
wangwendie <1621151730@qq.com>
2023-07-13 11:02:21 +0800

    去除dist文件的上传

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
-// import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
-const path = require("path");
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const nodeExternals = require('webpack-node-externals');
+const path = require("path");
 module.exports = {
   entry: './index.js',
+  target: 'node',
   externals: [nodeExternals()],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: 'commonjs2',
+  },
   resolve: {
     // 确保以下模块路径包含在解析的模块路径中
     modules: ['node_modules'],
     // 确保以下模块扩展名会被自动解析
-    extensions: [".ts", ".js", '.json'],
+    extensions: [".ts", ".js", '.json', ".mjs"],
     fallback: {
       fs: false,
     },
@@ -17,28 +21,16 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        exclude: [/node_modules/],
-        use: [
-          {
-            loader: "ts-loader",
-            options: {
-              transpileOnly: true,
-            },
-          },
-        ],
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
       },
     ],
   },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  stats: {
-    colors: true,
-    modules: true,
-    reasons: true,
-    errorDetails: true,
-  },
-  plugins: [new NodePolyfillPlugin()],
+  plugins: [],
 };
